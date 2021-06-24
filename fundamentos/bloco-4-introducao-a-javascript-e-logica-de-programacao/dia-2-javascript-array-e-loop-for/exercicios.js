dcc4c6db5d745dc052b7bc2f85494a2972b83494
RicardoAlFerreira <dieselferreira@gmail.com>
2021-06-24 19:41:11 -0300

    Coloquei a impressao para o lado de fora do loop

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -6,15 +6,22 @@ let sum = 0;
 
 
 //exercicio 2
-
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let sum = 0;
 //for (let index = 0; index < numbers.length; index += 1) {
-//   console.log (sum += numbers[index]);
+ //   sum += numbers[index]
 //}
-
+//console.log (sum);
 
 
 //exercicio 3
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
 for (let index = 0; index < numbers.length; index += 1) {
-      console.log ((sum += numbers[index])/10);
+    total = (sum += numbers[index])/10
 }
+console.log(total);
+
+//exercicio 4
+
 
