67750c0ef241e997dc73aa8164298b4b49de6a8d
RicardoAlFerreira <dieselferreira@gmail.com>
2021-06-24 19:22:36 -0300

    Exercicios 1, 2 e 3 concluidos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -1,1 +1,20 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+// exercicio 1
+//console.log(numbers);
+
+
+
+//exercicio 2
+
+//for (let index = 0; index < numbers.length; index += 1) {
+//   console.log (sum += numbers[index]);
+//}
+
+
+
+//exercicio 3
+for (let index = 0; index < numbers.length; index += 1) {
+      console.log ((sum += numbers[index])/10);
+}
+
