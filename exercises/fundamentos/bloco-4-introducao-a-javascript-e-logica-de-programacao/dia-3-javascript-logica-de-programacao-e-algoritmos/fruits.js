38ec1cec23600305ebc73b4c11e865f112996b3d
RicardoAlFerreira <dieselferreira@gmail.com>
2021-06-25 18:36:58 -0300

    Exercicio 1 concluido

--- exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/fruits.js
+++ exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/fruits.js
@@ -9,3 +9,18 @@ for (let index = 0; index < fruits.length; index += 1) {
  } else {
      console.log("Valor menor que 16");
  }
+
+ //Exercicio 1
+ let n = 5;
+ let quadrado = '*';
+ let inputline = '';
+
+ for(let index = 0; index < n; index += 1){
+     inputline = inputline + quadrado;
+ }
+
+ for(let index = 0; index < n; index +=1){
+     console.log(inputline);
+ 
+ }
+ 
