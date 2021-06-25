f12cb8cf22b442568ff59c807acc976fd71b0ba6
RicardoAlFerreira <dieselferreira@gmail.com>
2021-06-25 19:16:00 -0300

    Adicionei mais dois casos por curiosidade mas nao fui eu quem fez

--- exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/fruits.js
+++ exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/fruits.js
@@ -77,3 +77,36 @@ let ladoDireito = meioDoTriangulo;
     ladoEsquerdo -= 1;
 
 }
+
+//(desconsiderar,não fui eu quem fez!) Só pra ver como funciona
+let n = 7;
+let middle = (n + 1) / 2;
+let controlLeft = middle;
+let controlRight = middle;
+let symbol = '*';
+for (let line = 1; line <= middle; line += 1) {
+  let outputLine = '';
+  for (let col = 1; col <= n; col += 1) {
+    if (col == controlLeft || col == controlRight || line == middle) {
+      outputLine += symbol;
+    } else {
+      outputLine += ' ';
+    }
+  }
+  controlLeft -= 1;
+  controlRight += 1;
+  console.log(outputLine);
+}
+
+//Pra conferir se um numero é primo ou não!!! (Não fui eu quem fez!!)
+
+let divisors = 1;
+let numberToCheck = 30;
+
+for (let number = 2; number <= numberToCheck; number += 1) {
+  if (numberToCheck % number === 0) divisors += 1;
+}
+
+if (divisors === 2) console.log(numberToCheck + ' é primo');
+else console.log(numberToCheck + ' não é primo');
+
