3c418721e1b8246f716a36e30ad7d1e0a0e6c32a
RicardoAlFerreira <dieselferreira@gmail.com>
2021-06-28 18:33:45 -0300

    Exercicios 1 e 2 concluidos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercises.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercises.js
@@ -2,3 +2,22 @@
 // Exercícios 4.4
 
 // Exercício 1
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald'
+};
+
+console.log('Bem vinda,', info.personagem);
+
+// Exercicio 2
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+} ;
+
+console.log(info);
+
+//Exercicio 3
