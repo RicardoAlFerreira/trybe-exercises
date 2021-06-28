86b91be2b9896a0627450ea5a5626e3b0049184c
RicardoAlFerreira <dieselferreira@gmail.com>
2021-06-28 19:01:56 -0300

    Exercicios 4 e 5 concluidos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercises.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercises.js
@@ -21,3 +21,39 @@ let info = {
 console.log(info);
 
 //Exercicio 3
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+};
+for (let chave in info) {
+    console.log(info[chave]);
+}
+
+//Exercicio 4 e 5
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+};
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+};
+
+for (let propriedades in info) {
+    if (
+        propriedades === 'recorrente' &&
+        info[propriedades] === 'Sim' &&
+        info2[propriedades] === 'Sim'
+
+    ) {
+        console.log('Ambos recorrentes');
+    } else {
+        console.log(info[propriedades], 'e', info2[propriedades]);
+    }
+}
